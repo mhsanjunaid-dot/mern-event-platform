c1a87f664e9fe685adcad239128425341445c626
mhsanjunaid-dot <mhsanjunaid@gmail.com>
2025-12-18 12:01:35 +0530

    fixing bugs

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://mern-event-platform-qgpq.onrender.com/api",
-  headers: { "Content-Type": "application/json" }
+  baseURL: "https://mern-event-platform-qgpq.onrender.com/api", 
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 axiosInstance.interceptors.request.use((config) => {
