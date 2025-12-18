9d2171460ff9b7f0cc14f12946e287065d0b3858
mhsanjunaid-dot <mhsanjunaid@gmail.com>
2025-12-18 15:28:01 +0530

    Fix: Improve FormData Content-Type handling in axios

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -9,9 +9,7 @@ axiosInstance.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  if (config.data instanceof FormData) {
-    delete config.headers["Content-Type"];
-  } else {
+  if (!(config.data instanceof FormData)) {
     config.headers["Content-Type"] = "application/json";
   }
   return config;
